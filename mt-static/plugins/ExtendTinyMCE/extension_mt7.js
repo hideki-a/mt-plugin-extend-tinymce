1101f29949c514c2c6ff7a81e70f9c890c7f3f09
Hideki Abe <hideki.abe.0630@gmail.com>
2018-10-12 19:52:03 +0900

    MT7向けの設定を定義

--- mt-static/plugins/ExtendTinyMCE/extension_mt7.js
+++ mt-static/plugins/ExtendTinyMCE/extension_mt7.js
@@ -4,24 +4,24 @@ var config   = MT.Editor.TinyMCE.config;
 var base_url = StaticURI + 'plugins/ExtendTinyMCE/';
 
 // TinyMCEで利用するプラグイン
-// http://www.tinymce.com/wiki.php/Configuration3x:plugins
-var add_plugins = ',table';
+var add_plugins = ',table,template';
 
 // ボタン - 1段目
-// http://www.tinymce.com/wiki.php/Configuration3x:theme_advanced_buttons_1_n
+// https://www.tiny.cloud/docs/advanced/editor-control-identifiers/#toolbarcontrols
 var buttons1;
 if (config.plugin_mt_wysiwyg_buttons1.indexOf('template') > -1) {
-    buttons1 = (config.plugin_mt_wysiwyg_buttons1 || '') + ',|,table';
+    buttons1 = config.plugin_mt_wysiwyg_buttons1 + ',|,table';
 } else {
-    buttons1 = (config.plugin_mt_wysiwyg_buttons1 || '') + ',|,table,template';
+    buttons1 = config.plugin_mt_wysiwyg_buttons1 + ',|,template,|,table';
 }
 
 // ボタン - 2段目
-var buttons2 = (config.plugin_mt_wysiwyg_buttons2 || '') + ',fontsizeselect';
+var buttons2 = config.plugin_mt_wysiwyg_buttons2.replace('|,mt_fullscreen', 'fontsizeselect,|,mt_fullscreen');
+// var buttons2 = config.plugin_mt_wysiwyg_buttons2.replace('|,mt_fullscreen', 'styleselect,fontsizeselect,|,mt_fullscreen');
 
 // スタイルプルダウンの定義
 // 任意のstyle属性値やclass属性値を付与した要素が挿入可能になる
-// http://www.tinymce.com/wiki.php/Configuration3x:style_formats
+// https://www.tiny.cloud/docs/configure/content-formatting/#style_formats
 // var styles = [
 //         { title: 'セクション', block: 'section', wrapper: true, merge_siblings: false },
 //         { title: '画像ブロック', block: 'div', wrapper: true, merge_siblings: false, classes: 'clearfix' }
@@ -34,8 +34,8 @@ var fontsize_formats = '12px 14px 16px 18px 20px';
 
 // 書式プルダウンの定義
 // カンマ区切りで要素を指定
-// http://www.tinymce.com/wiki.php/Configuration3x:theme_advanced_blockformats
-// var theme_advanced_blockformats = 'p,h2,h3,h4,h5';
+// https://www.tiny.cloud/docs/configure/content-formatting/#block_formats
+// var blockformats = '段落=p;見出し2=h2;見出し3=h3';
 
 // テンプレートリスト
 // 記事の定型文を設定した場合、記事編集画面では定型文がリストアップされます。
@@ -59,12 +59,9 @@ $.extend(config, {
     plugins: config.plugins + add_plugins,
     plugin_mt_wysiwyg_buttons1: buttons1,
     plugin_mt_wysiwyg_buttons2: buttons2,
-    // plugin_mt_wysiwyg_buttons3: buttons3,
-    // indent_before: indent_before,
-    // indent_after: indent_after,
     // style_formats: styles,
     fontsize_formats: fontsize_formats,
-    // theme_advanced_blockformats: theme_advanced_blockformats,
+    // block_formats: blockformats,
     templates: tmpl_list,
     convert_urls: convert_urls,
     remove_script_host: remove_script_host,
