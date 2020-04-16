11834ced07907c441819d115168a94bd3ab0dc04
Hideki Abe <hideki.abe.0630@gmail.com>
2020-04-16 19:26:59 +0900

    テンプレート一覧に関する補足を記述

--- mt-static/plugins/ExtendTinyMCE/tmpl/template_list.js
+++ mt-static/plugins/ExtendTinyMCE/tmpl/template_list.js
@@ -1,3 +1,5 @@
+// このファイルはMT6以下の場合に使用します。
+// MT7の場合はextension_mt7.js内、「テンプレートリスト」に記述します。
 var tmpl_base_url = parent.window.StaticURI + 'plugins/ExtendTinyMCE/tmpl/',
     tinyMCETemplateList = new Array(
     // [Name, URL],
